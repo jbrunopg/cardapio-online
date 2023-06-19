a09395fc4549e7b49f5b682f7b9f69b8310eb512
Bruno Palhano <107354811+jbrunopg@users.noreply.github.com>
2023-06-18 23:35:15 -0300

    Configuração Food.Java

--- src/main/java/com/example/cardapio/food/Food.java
+++ src/main/java/com/example/cardapio/food/Food.java
@@ -1,8 +1,17 @@
 package com.example.cardapio.food;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 
-@Table
+
+@Table(name = "foods")
+@Entity(name = "foods")
 public class Food {
-
+    @Id @GenerateValue(strategy = Generation.IDENTITY)
+    private Long id;
+    private String title;
+    private String image;
+    private Integer price;
 }
