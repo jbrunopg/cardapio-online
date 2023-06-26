fe2d2483e943bcfb46449deb517d8bd59bd005bd
Bruno Palhano <107354811+jbrunopg@users.noreply.github.com>
2023-06-25 23:52:09 -0300

    Configuração Food.Java

--- src/main/java/com/example/cardapio/controller/FoodControler.java
+++ src/main/java/com/example/cardapio/controller/FoodControler.java
@@ -1,16 +1,23 @@
 package com.example.cardapio.controller;
 
+import com.example.cardapio.food.Food;
+import com.example.cardapio.food.FoodRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
 @RequestMapping("food")
 public class FoodControler {
+
+    private FoodRepository repository;
     @GetMapping
     public void getAll(){
 
+        List<Food> foodList = repository
     }
 }
 
