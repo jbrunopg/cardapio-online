955a941d1dcaae98b77af38d8ab5e6fdea190105
Bruno Palhano <brunopalhanog@gmail.com>
2023-09-16 00:39:03 -0300

    Configuração DTO

--- src/main/java/com/example/cardapio/controller/FoodControler.java
+++ src/main/java/com/example/cardapio/controller/FoodControler.java
@@ -2,7 +2,9 @@ package com.example.cardapio.controller;
 
 import com.example.cardapio.food.Food;
 import com.example.cardapio.food.FoodRepository;
+import com.example.cardapio.food.FoodResponseDTO;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,10 +16,16 @@ import java.util.List;
 public class FoodControler {
 
     private FoodRepository repository;
-    @GetMapping
-    public void getAll(){
 
-        List<Food> foodList = repository
+    @PostMapping
+    public void saveFood(){
+
+    }
+    @GetMapping
+    public List<FoodResponseDTO> getAll(){
+
+        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new);
+        return foodList;
     }
 }
 
---
+++ src/main/java/com/example/cardapio/food/FoodResponseDTO.java
@@ -0,0 +1,7 @@
+package com.example.cardapio.food;
+
+public class FoodResponseDTO(Long id, String title, String image, Integer price) {
+    public FoodResponseDTO(Food food){
+        this(food.getId(), food.getTitle(), food.getImage(), food.getPrice());
+    }
+}
