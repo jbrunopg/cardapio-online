1399d0492c9ea11e581e874e18e82e9e84496ef8
Bruno Palhano <brunopalhanog@gmail.com>
2023-09-18 00:41:21 -0300

    Configuração DTO

--- src/main/java/com/example/cardapio/food/Food.java
+++ src/main/java/com/example/cardapio/food/Food.java
@@ -21,4 +21,10 @@ public class Food {
     private String image;
 
     private Integer price;
+
+    public Food(FoodRequestDTO data){
+        this.image = data.image();
+        this.price = data.price();
+        this.title = data.title();
+    }
 }
